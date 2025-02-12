28bc7f26a7cd62903a3cb6e2b9be5d221eb96a06
Valentina Larrañaga <137834433+mvlarra@users.noreply.github.com>
2025-02-12 16:30:38 +0000

    InProgress

--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
 import os
+from dotenv import load_dotenv
+
 import pandas as pd
 import seaborn as sns
-from dotenv import load_dotenv
 
 # load the .env file variables
 load_dotenv()
+client_id = os.environ.get('CLIENT_ID')
+client_secret = os.environ.get('CLIENT_SECRET')
